a4c507a9e870beea6a017c39a8267c45e0526e58
Ricardo Medina <rmedina0531@gmail.com>
2021-10-07 18:45:56 -0700

    Initial Commit. Working servers for example web service

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const OtherPage = () => {
+    return (
+        <div>
+            Im Some other Page!
+            <Link to="/">Go back Home</Link>
+        </div>
+    )
+}
+
+export default OtherPage
